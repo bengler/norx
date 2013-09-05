0752e38319d830aee625bee73c1e44f067dd7a3b
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2013-09-05 23:13:24 +0200

    Update base setup

--- sh/base_setup.sh
+++ sh/base_setup.sh
@@ -12,13 +12,14 @@ locale-gen en_US.UTF-8
 
 if [ ! -f '/home/kartverk/.done_packages' ]; then
 	echo  "Installing needed packages to handle map data"
-	# Packages
 
+	# Add some repositories
 	apt-add-repository -y ppa:sharpie/for-science
 	apt-add-repository -y  ppa:sharpie/postgis-nightly
 	add-apt-repository -y ppa:mapnik/nightly-2.0
 	add-apt-repository -y ppa:chris-lea/node.js
 
+	# Update package list for new repos.
 	apt-get update
 
 	## Install postgreqsql database
@@ -32,14 +33,11 @@ if [ ! -f '/home/kartverk/.done_packages' ]; then
 	# Install mapnik
 	sudo apt-get -y install libmapnik2-2.0 libmapnik2-dev
 
-	## Install git, unzip, subversion and zerofree
-	apt-get -y install git unzip subversion zerofree
+	## Install git, unzip, subversion, zerofree and node
+	apt-get -y install git unzip subversion zerofree nodejs
 
 	# Install needed Python packages
 	apt-get install -y libboost-python-dev python-pip python-dev
-
-	## Install node
-	apt-get -y install nodejs
 
 	# Install python modules
 	apt-get -y install python-software-properties
@@ -52,7 +50,7 @@ if [ ! -f '/home/kartverk/.done_postgres' ]; then
 	echo  "Setting up database"
 	# Postgres/PostGIS setup
 
-	echo  "\t * Creating PostGIS template"
+	echo  "    * Creating PostGIS template"
 	## Create PostGIS template
 	sudo -u postgres createdb -E UTF8 template_postgis2
 	sudo -u postgres createlang -d template_postgis2 plpgsql
@@ -65,7 +63,7 @@ if [ ! -f '/home/kartverk/.done_postgres' ]; then
 	sudo -u postgres psql -d template_postgis2 -c "GRANT ALL ON geography_columns TO PUBLIC;"
 	sudo -u postgres psql -d template_postgis2 -c "GRANT ALL ON spatial_ref_sys TO PUBLIC;"
 
-	echo  "\t * Creating user 'kartverk' with password 'bengler'"
+	echo  "    * Creating user 'kartverk' with password 'bengler'"
 	## Create kartverk user, role and database
 	sudo -u postgres psql -c "CREATE ROLE root LOGIN INHERIT CREATEDB;"
 	sudo -u postgres psql -c "ALTER USER root WITH PASSWORD 'bengler';"
@@ -73,7 +71,7 @@ if [ ! -f '/home/kartverk/.done_postgres' ]; then
 	sudo -u postgres psql -c "CREATE ROLE kartverk LOGIN INHERIT CREATEDB;"
 	sudo -u postgres psql -c "ALTER USER kartverk WITH PASSWORD 'bengler';"
 	#sudo -u postgres psql -c "GRANT kartverk TO kartverk;"
-	echo  "\t * Creating database 'kartverk'"
+	echo  "    * Creating database 'kartverk'"
 	sudo -u kartverk createdb -O kartverk -E UTF8 -T template_postgis2 kartverk
 
 	sudo -u kartverk touch '/home/kartverk/.done_postgres'
@@ -87,7 +85,7 @@ git config --global user.email tildeniels1@gmail.com
 
 if [ ! -f '/home/kartverk/.done_dataseed' ]; then
 	echo  "Seeding map data. This will take a very long time!"
-	echo "\t * Generating and activating humongous (25 GB) swapfile needed to parse BIG GeoJSON files"
+	echo "    * Generating and activating humongous (25 GB) swapfile needed to parse BIG GeoJSON files"
 
 	# Create swapfile of 25GB with block size 1MB
 	dd if=/dev/zero of=/swapfile bs=1024 count=26214400
@@ -99,28 +97,28 @@ if [ ! -f '/home/kartverk/.done_dataseed' ]; then
 	swapon /swapfile
 
 	# Prepare to cook map data
-	echo "\t * Fetching seed code from GitHub"
+	echo "    * Fetching seed code from GitHub"
 
 	cd /home/kartverk
 	sudo -u kartverk git clone "https://$GITHUB_ACCESS_TOKEN@github.com/bengler/kartverk_data_seed"
 	cd /home/kartverk/kartverk_data_seed
 
-	echo "\t * Starting seed. Please be patient..."
+	echo "    * Starting seed. Please be patient..."
 
 	sudo -u kartverk ./seed.sh kartverk kartverk bengler
 
-	echo "\t * Deactivatin and removing humongous swap file"
+	echo "    * Deactivatin and removing humongous swap file"
 	swapoff /swapfile
 	rm -rf /swapfile
 	sudo -u kartverk touch '/home/kartverk/.done_dataseed'
-	echo "\t * Seed done!"
+	echo "   * Seed done!"
 fi
 
 if [ ! -f '/home/kartverk/.done_services' ]; then
 	echo  "Setting up map services to run locally"
 	# Set up services that we need running
 	cd /home/kartverk
-	echo  "\t * Fetching service code from GitHub"
+	echo  "    * Fetching service code from GitHub"
 	sudo -u kartverk git clone "https://$GITHUB_ACCESS_TOKEN@github.com/bengler/kartverk_vm_services"
 	cd /home/kartverk/kartverk_vm_services
 	sudo -u kartverk ./bootstrap.sh
